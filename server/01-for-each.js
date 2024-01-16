e614d675252e1b83c158e8870bd5c83d19a8d398
Andrea Rivas <andrerivas25@gmail.com>
2024-01-16 15:51:34 -0300

    Metodo For Each

---
+++ server/01-for-each.js
@@ -0,0 +1,10 @@
+const letters = ['a', 'b', 'c'];
+
+// Ciclo for
+for (let index = 0; index < letters.length; index++) {
+    const element = letters[index];
+    console.log(element);
+}
+
+// For each
+letters.forEach(item => console.log(item));
